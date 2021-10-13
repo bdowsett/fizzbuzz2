7a3a23e57fb309973429c31e7575e2640d26a901
Ben Dowsett <bendowsett@gmail.com>
2021-10-13 11:25:10 +0000

    third test added and passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
 def fizzbuzz(number)
-    if number % 3 == 0
-        'fizz'
+    if number % 3 == 0 && number % 5 == 0
+        'fizzbuzz'
+    elsif number % 3== 0
+        "fizz"
     elsif number % 5 == 0
         'buzz'
     end
